6f190b6ef66f6a0d6bf06f619b8850abf0302fc2
kurokuriboh <hiepcan22@gmail.com>
2016-01-07 01:25:09 -0800

    Built the initial state of Flicks and updated README

---
+++ Flicks/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  Flicks
+//
+//  Created by Hiep V. Can on 1/6/16.
+//  Copyright © 2016 Hiep Can. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
